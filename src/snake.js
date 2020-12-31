feecf2b822dce9e7dcf8ee172857100589cbd412
Bryan Rhodunda <rhodunda@udel.edu>
2020-12-31 16:48:08 -0500

    updated movement stage 1

--- src/snake.js
+++ src/snake.js
@@ -6,13 +6,33 @@ class snake extends Component {
         length: 0,
         modifier: 5,
         head: [],
-        position: (10,10)
+        position: {
+            top: 10,
+            left: 10
+        },
+        currentDirection: "down "
     }
 
     componentDidMount() {
         document.addEventListener('keydown', event => { 
-            if(event.keyCode === 38) {
-               // got key codes to work 
+
+            switch(event.keyCode) {
+            case 37:
+                // styles.left = parseInt(styles.left) - this.state.modifier + 'px'
+                console.log('left')
+                console.log(styles.left)
+                break;
+            case 38:
+                console.log('up')
+                break;
+            case 39:
+                console.log('right')
+                break;
+            case 40:
+                console.log('down')
+                break;
+
+
             }
         })
     }
@@ -22,7 +42,7 @@ class snake extends Component {
 
         
     return (
-        <div className={styles.snake}></div>
+        <div className={styles.snake} style={{top : this.state.position.top}, {left : this.state.position.left}}></div>
     )
     }
 }
