df53fccfd77b872e2327525e26639e3682c2cbc5
Bryan Rhodunda <rhodunda@udel.edu>
2020-12-31 15:47:44 -0500

    updated snake added click event keycodes now working

--- src/snake.js
+++ src/snake.js
@@ -3,11 +3,26 @@ import styles from './snake.module.css'
 
 class snake extends Component {
     state = {
-        length: 0
+        length: 0,
+        modifier: 5,
+        head: [],
+        position: (10,10)
     }
+
+    componentDidMount() {
+        document.addEventListener('keydown', event => { 
+            if(event.keyCode === 38) {
+               // got key codes to work 
+            }
+        })
+    }
+
+
     render() {
+
+        
     return (
-<div className={styles.snake}></div>
+        <div className={styles.snake}></div>
     )
     }
 }
