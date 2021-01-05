a62b159bc28e3011d2b1d7625ceee3bbe9ffbab5
Bryan Rhodunda <rhodunda@udel.edu>
2021-01-05 00:12:04 -0500

    updated grid

--- src/Hoc/Layout.js
+++ src/Hoc/Layout.js
@@ -5,18 +5,46 @@ import Snake from '../snake'
 
 class Layout extends Component {
     state = {
-        row: 15,
-        colum: 15,
+        row: 10,
+        colum: 10,
         grid: []
+    };
+
+    componentWillMount () {
+        this.gridHandler()
     }
 
-    render() {
-        return(
-            <div className={styles.board}>
-                <Food></Food>
-                <Snake></Snake>
+ gridHandler = () => {
+    const grid = [];
+        for(let i = 0; i < this.state.row; i++) {
+            for(let j = 0; j < this.state.colum; j++) {
+                grid.push({
+                    i,
+                    j,
+                })
+            }
+        }
+        this.setState({grid: grid})
+    }
+    
 
+
+    render() {
+
+
+        const gridItems = this.state.grid.map((grid) => {
+            return <div className={styles.gridItem}/>
+        })
+        return(
+
+            <div className={styles.snakeContainer}>
+                <div className={styles.grid}>{gridItems}</div>
             </div>
+            // <div className={styles.board}>
+            //     <Food></Food>
+            //     <Snake></Snake>
+
+            // </div>
             
         )
     }
