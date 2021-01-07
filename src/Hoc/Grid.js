d915e6532b6449d7758e5fbeb002fef20248c78e
Bryan Rhodunda <rhodunda@udel.edu>
2021-01-06 19:41:49 -0500

    added the snake to the grid

---
+++ src/Hoc/Grid.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react'
+import styles from './Layout.module.css'
+import Snake from '../snake'
+import Food from '../food'
+
+
+class Grid extends Component {
+    state = {
+        row: 10,
+        colum: 10,
+        grid: []
+    };
+
+    componentWillMount () {
+        this.gridHandler()
+    }
+
+ gridHandler = () => {
+    const grid = [];
+        for(let i = 0; i < this.state.row; i++) {
+            for(let j = 0; j < this.state.colum; j++) {
+                grid.push({
+                    i,
+                    j,
+                })
+            }
+        }
+        this.setState({grid: grid})
+    }
+    
+
+
+    render() {
+
+
+        const gridItems = this.state.grid.map((grid) => {
+            return <div className={styles.gridItem}/>
+        })
+        return(
+
+            <div className={styles.snakeContainer}>
+                <div className={styles.grid}>{gridItems} 
+                <Snake/>
+                </div>
+                
+            </div>
+            
+        )
+    }
+
+
+}
+
+export default Grid
--- src/Hoc/Layout.js
+++ src/Hoc/Layout.js
@@ -1,55 +1,17 @@
 import React, { Component } from 'react'
-import styles from './Layout.module.css'
-import Food from '../food'
-import Snake from '../snake'
+import Grid from './Grid'
+// import Snake from '../snake'
+// import Food from '../food'
 
 class Layout extends Component {
-    state = {
-        row: 10,
-        colum: 10,
-        grid: []
-    };
 
-    componentWillMount () {
-        this.gridHandler()
-    }
-
- gridHandler = () => {
-    const grid = [];
-        for(let i = 0; i < this.state.row; i++) {
-            for(let j = 0; j < this.state.colum; j++) {
-                grid.push({
-                    i,
-                    j,
-                })
-            }
-        }
-        this.setState({grid: grid})
-    }
-    
-
-
-    render() {
-
-
-        const gridItems = this.state.grid.map((grid) => {
-            return <div className={styles.gridItem}/>
-        })
-        return(
-
-            <div className={styles.snakeContainer}>
-                <div className={styles.grid}>{gridItems}</div>
-            </div>
-            // <div className={styles.board}>
-            //     <Food></Food>
-            //     <Snake></Snake>
-
-            // </div>
-            
-        )
-    }
-
-
+render(){
+    return (
+      
+        <Grid>
+        </Grid>
+    )
+   }
 }
 
 export default Layout
